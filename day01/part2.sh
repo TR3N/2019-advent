d884edf0daa976b72f5bba83b776a12bd515acb5
Dan Terenick <dan.terenick@gmail.com>
2019-12-03 16:39:38 -0500

    added day 2

--- day01/part2.sh
+++ day01/part2.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 # TR3N
 #
+
 fuel=0
+initialfuelmass=""
+
 for mass in $(cat input)
   do
   X=$((mass / 3 - 2))
   fuel=$((fuel + X))
+  initialfuelmass="$initialfuelmass $X"
   done
-echo $fuel
 
+for i in $initialfuelmass
+  do
+  Z=0
+  while [ $i -gt 0 ]; 
+    do
+    Z=$((i / 3 - 2))
+    if [ $Z -ge 0 ]; then
+      Y=$((Y+Z))
+    fi
+    i=$((i / 3 - 2))
+    done
+  done
+  echo -ne "$fuel + $Y = "; echo $((fuel + Y))
