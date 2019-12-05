2d4980374751262744e82060b694f087178f1a6e
Dan Terenick <dan.terenick@gmail.com>
2019-12-05 16:19:44 -0500

    nasty

--- day02/part2.sh
+++ day02/part2.sh
@@ -1,38 +1,75 @@
 #!/bin/bash
 # TR3N
 #
+#set -x
 declare -a a_seq
-
-i=0
+refresh_array () {
+zz=0
 for x in $(cat input | tr ',' ' ')
   do
-  a_seq[$i]=$x
-  i=$((i+1))
+  a_seq[$zz]=$x
+  zz=$((zz+1))
   done
-  a_seq[1]=12
-  a_seq[2]=2
+}
 
+main () {
 i=0
 while true
   do
-  opcode=${a_seq[$i]}
-  if [ "$opcode" == "99" ]; then
-    echo ${a_seq[0]}
-    exit
+    opcode=${a_seq[$i]}
+    if [ "$opcode" == "99" ]; then
+       if [ "${a_seq[0]}" == "19690720" ]; then
+         THENUMBER=$((100 * $inoun + $iverb))
+         echo "noun:$inoun verb:$iverb number:$THENUMBER"
+         exit
+        fi
+      break
+    fi
+    numX=${a_seq[$i+1]}
+    numY=${a_seq[$i+2]}
+    place=${a_seq[$i+3]}
+    case $opcode in
+      1)
+      value=$((a_seq[$numX] + a_seq[$numY]))
+      a_seq[$place]=$value
+      i=$((i+4))
+      ;;
+      2)
+      value=$((a_seq[$numX] * a_seq[$numY]))
+      a_seq[${place}]=${value}
+      i=$((i+4))
+      ;;
+      *)
+      break 
+      ;;
+    esac
+  done
+}
+
+
+#  a_seq[1]=12
+#  a_seq[2]=2
+
+inoun=0
+iverb=0
+while true
+  do
+  a_seq[1]=$inoun
+    while true
+      do
+      refresh_array
+      a_seq[1]=$inoun
+      a_seq[2]=$iverb
+      main
+      iverb=$((iverb + 1))
+      if [ $iverb -ge 100 ]; then 
+        break
+      fi
+      a_seq[1]=$inoun
+      done
+  inoun=$((inoun + 1))
+  iverb=0
+  if [ $inoun -ge 100 ]; then
+    break
   fi
-  numX=${a_seq[$i+1]}
-  numY=${a_seq[$i+2]}
-  place=${a_seq[$i+3]}
-  case $opcode in
-    1)
-    value=$((a_seq[$numX] + a_seq[$numY]))
-    a_seq[$place]=$value
-    i=$((i+4))
-    ;;
-    2)
-    value=$((a_seq[$numX] * a_seq[$numY]))
-    a_seq[${place}]=${value}
-    i=$((i+4))
-    ;;
-  esac
   done
